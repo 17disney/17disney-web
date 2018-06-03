815c96d896367fcdcd89d53abd827bb1bd8a348b
xank <xank@qq.com>
2018-06-03 11:26:33 +0800

    add fundebug

--- src/main.js
+++ src/main.js
@@ -32,3 +32,31 @@ new Vue({
   components: { App },
   template: '<App/>'
 })
+
+function formatComponentName(vm) {
+  if (vm.$root === vm) return 'root'
+
+  var name = vm._isVue
+    ? (vm.$options && vm.$options.name) ||
+      (vm.$options && vm.$options._componentTag)
+    : vm.name
+  return (
+    (name ? 'component <' + name + '>' : 'anonymous component') +
+    (vm._isVue && vm.$options && vm.$options.__file
+      ? ' at ' + (vm.$options && vm.$options.__file)
+      : '')
+  )
+}
+
+Vue.config.errorHandler = function(err, vm, info) {
+  var componentName = formatComponentName(vm)
+  var propsData = vm.$options && vm.$options.propsData
+
+  fundebug.notifyError(err, {
+    metaData: {
+      componentName: componentName,
+      propsData: propsData,
+      info: info
+    }
+  })
+}
