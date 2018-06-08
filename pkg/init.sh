5b89f940d5c63ddcfab086a5f785211e6dec0881
xank <xank@qq.com>
2018-06-08 23:19:47 +0800

    add sh

--- pkg/init.sh
+++ pkg/init.sh
@@ -1,4 +1,5 @@
 cd ./src/package
+git clone https://github.com/17disney/17disney-common
 git clone https://github.com/17disney/17disney-language
 git clone https://github.com/17disney/dm-ui
 git clone https://github.com/17disney/att-ui
--- pkg/update.sh
+++ pkg/update.sh
@@ -1,4 +1,5 @@
 cd ./src/package
+git pull https://github.com/17disney/17disney-common
 git pull https://github.com/17disney/17disney-language
 git pull https://github.com/17disney/dm-ui
 git pull https://github.com/17disney/att-ui
