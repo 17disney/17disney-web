52e72ad35478d6fda549f19d78954d11e6c31629
xank <xank@qq.com>
2019-01-03 23:50:51 +0800

    update package

--- src/components/Calendar/CalendarItemTicket.vue
+++ src/components/Calendar/CalendarItemTicket.vue
@@ -1,52 +1,18 @@
-<style lang='stylus'>
-@require '~@/styles/disney/var/color.styl';
-
-.popover-att {
-  padding: 10px 0 !important;
-  text-align: center;
-
-  &__charts {
-    width: 200px !important;
-    margin: 10px auto;
-    margin-top: 15px;
-  }
-
-  &__schedule {
-    color: $color-grey;
-  }
-
-  &__num {
-    display: flex;
-    width: 185px;
-    margin: 0 auto;
-    margin-bottom: 10px;
-    font-size: 13px;
-    color: $color-grey;
-  }
-
-  &__wait {
-    flex: 1;
-    text-align: left;
-  }
-}
-</style>
-
 <template>
   <div
     class="calendar-item"
     :class="[className]"
   >
-    <!-- {{data.availableCount}} -->
     <p class="calendar-item__day">{{day}}</p>
-    <div class="badge"></div>
+    <transition>
+      <div class="badge"></div>
+    </transition>
   </div>
-
 </template>
 
 <script>
 import { TICKET_INDEX_MAP } from '@/common/data/const'
 import ChartsAttBase from '@/components/Charts/ChartsAttBase'
-
 
 export default {
   components: { ChartsAttBase },
@@ -62,21 +28,14 @@ export default {
     }
   },
 
-  data() {
-    return {
-      tipDisabled: true,
-      id: Math.random()
-    }
-  },
-
   computed: {
     className() {
-      const {ticketNum = 0} = this.data
+      const { ticketNum = 0 } = this.data
       const value = ticketNum
       let color
 
       TICKET_INDEX_MAP.forEach(item => {
-        const {gt, lte, className: _color} = item
+        const { gt, lte, className: _color } = item
 
         if (gt && lte) {
           if (value >= gt && value <= lte) {
@@ -86,9 +45,6 @@ export default {
           if (value >= gt) color = _color
         }
       })
-
-      console.log(color)
-
       return color
     }
   }
